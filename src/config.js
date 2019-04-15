09f6c5ed80bff1ab55e86ef7da12603b0f062ab6
Christopher <christopher.carnivale@gmail.com>
2019-04-15 17:44:51 -0400

    changed api location settings

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  REACT_APP_API_BASE: process.env.REACT_APP_API_BASE || 'http://localhost:8000/api',
-  TOKEN_KEY: 'blogful-client-auth-token'
+  API_ENDPOINT: process.env.REACT_APP_API_BASE || 'http://localhost:8000/api',
+  TOKEN_KEY: 'spaced-repetition-token'
 }
