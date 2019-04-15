61706e6e114ba5f3d49b74812e2491e17578c09a
Christopher <christopher.carnivale@gmail.com>
2019-04-15 17:36:10 -0400

    deployment changes

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
-  TOKEN_KEY: 'blogful-client-auth-token',
+  REACT_APP_API_BASE: process.env.REACT_APP_API_BASE || 'http://localhost:8000/api',
+  TOKEN_KEY: 'blogful-client-auth-token'
 }
