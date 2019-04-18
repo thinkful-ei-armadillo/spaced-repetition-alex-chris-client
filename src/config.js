d5007bb78c476c6e979b91f36ab32052d5eef98d
Christopher <christopher.carnivale@gmail.com>
2019-04-18 10:26:13 -0400

    changed server for deploy

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api'|| process.env.REACT_APP_API_BASE,
+  API_ENDPOINT: process.env.REACT_APP_API_BASE || 'http://localhost:8000/api',
   TOKEN_KEY: 'spaced-repetition-client-auth-token'
 }
